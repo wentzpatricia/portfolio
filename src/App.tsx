f39315deba6b9b46a5488a2db31d2f5070282729
wentzpatricia <patriciawentzm@gmail.com>
2025-02-12 18:13:23 -0300

    feat: adicionando estrutura basica

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-sky-800 underline">Texto</h1>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 rounded-2xl">
+        Submit
+      </button>
+    </>
+  );
+}
+
+export default App;
