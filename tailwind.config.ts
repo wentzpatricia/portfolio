7e723a0b9b248cfc8fc8160ad5f57c840d67b29d
wentzpatricia <patriciawentzm@gmail.com>
2025-03-09 21:00:35 -0300

    feat: adicionando home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,17 @@ module.exports = {
         extend: {
           animation: {
             'gradient': 'gradient 30s linear infinite',
+            'rotate-diagonal': 'rotateDiagonal 10s infinite linear',
           },
           keyframes: {
             'gradient': {
               to: { 'background-position': '200% center' },
-            }
+            },
+            'rotateDiagonal': {
+              '0%': { transform: 'rotate(0deg) translate(20px, -20px)' }, 
+              '100%': { transform: 'rotate(360deg) translate(20px, -20px)' },
+            },
+          
           },                  
         },
       },
